4f9c58f96de4119d2ec2c914a057e066dbbb97d9
Denis Valdivieso <sshshaka@gmail.com>
2022-08-03 21:39:58 -0400

    add getStaticProps - pages/blog/index

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,14 +4,17 @@ import styles from '../styles/Nav.module.css'
 export default function Nav() {
   return (
     <nav className={styles.nav}>
+      <Link href='/'>
+        <a>home</a>
+      </Link>
+      <Link href='/blog'>
+        <a>blog</a>
+      </Link>
       <Link href='/about'>
         <a>about</a>
       </Link>
       <Link href='/contact'>
         <a>contact</a>
-      </Link>
-      <Link href='/blog'>
-        <a>blog</a>
       </Link>
     </nav>
   )
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,30 @@
 import Layout from '../../components/Layout'
 
-export default function index() {
+export default function index({ data }) {
+  if (!data) return
   return (
     <Layout title='Blog | Next.js'>
       <h2>Blog</h2>
+      {data
+        .filter((d) => d.id <= 20)
+        .map((d) => (
+          <div key={d.id}>
+            <h3>{d.title}</h3>
+            <p>{d.body}</p>
+          </div>
+        ))}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.json()
+    return {
+      props: { data }
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
